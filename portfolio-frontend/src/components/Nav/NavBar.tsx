18696ea5de9681ca423ff876115fa7c5fc67cfbb
Brandon Ryan <26101249+erised11@users.noreply.github.com>
2024-05-16 11:56:31 +0200

    Added resizing for logo

--- portfolio-frontend/src/components/Nav/NavBar.tsx
+++ portfolio-frontend/src/components/Nav/NavBar.tsx
@@ -6,7 +6,7 @@ import logo from "../../assets/aperture3.png";
 const NavBar = () => {
   return (
     <div className="h-32 text-gray-300 flex justify-between tracking-widest px-10 py-10">
-      <img src={logo} />
+      <img src={logo} className="h-2/3 md:h-4/5 lg:h-full" />
       <div className="hidden sm:flex gap-10">
         <div>
           <Link to={"/"}>
